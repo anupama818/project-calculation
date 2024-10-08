d3409a46e53f003ddefdb81706461c0309828b7e
anupama818 <an7293b@gmail.com>
2024-10-08 12:09:14 +0530

    program for addition

--- add.py
+++ add.py
@@ -1,6 +1,6 @@
 
-def add_numbers(a, b):
-    return  a+b
+def add_numbers(e, f):
+    return  e+f
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
