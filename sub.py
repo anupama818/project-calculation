5b5e358bea2b75368cac47401fb628d18bd72afc
anupama818 <an7293b@gmail.com>
2024-10-08 10:34:30 +0530

    this is my 2nd version

---
+++ sub.py
@@ -0,0 +1,11 @@
+
+def subtract_numbers(a, b):
+    return a - b
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+result = subtract_numbers(num1, num2)
+print("The difference between {} and {} is {}".format(num1, num2, result))
